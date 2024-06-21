f280a9a1e594da4e922a1cb4626610447f12e657
I Made Tara Bujawan <147168301+Bujawan22@users.noreply.github.com>
2024-06-21 21:00:35 +0800

    Update CardsHandler.java

--- TugasPBO2/src/main/java/SubscriptionAPI/server/CardsHandler.java
+++ TugasPBO2/src/main/java/SubscriptionAPI/server/CardsHandler.java
@@ -1,4 +1,28 @@
 package SubscriptionAPI.server;
-
+import java.sql.SQLException;
+import org.json.JSONObject;
+import SubscriptionAPI.sketch.Cards;
+import SubscriptionAPI.presistence.CardsDO;
 public class CardsHandler {
+    CardsDO cardsDO = new CardsDO();
+    private Cards parseCardsFromJSON(JSONObject jsonObject) {
+        Cards cards = new Cards();
+        cards.setId(jsonObject.optInt("id"));
+        cards.setCustomer(jsonObject.optInt("customer"));
+        cards.setCard_type(jsonObject.optString("card_type"));
+        cards.setMasked_number(jsonObject.optString("masked_number"));
+        cards.setExpiry_month(jsonObject.optInt("expiry_month"));
+        cards.setExpiry_year(jsonObject.optInt("expiry_yeaar"));
+        cards.setStatus(jsonObject.optString("status"));
+        cards.setIs_primary(jsonObject.optInt("is_primary"));
+        return cards;
+    }
+
+    public String deleteCardsIfNotPrimary(String[] path) throws SQLException {
+        // Parse customer ID and card ID from the path
+        int customerId = Integer.parseInt(path[2]); // Assuming /cards/customerId/cardId
+        int cardId = Integer.parseInt(path[4]);
+
+        return cardsDO.deleteCardIfNotPrimary(customerId, cardId);
+    }
 }
